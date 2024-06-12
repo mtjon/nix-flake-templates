c670fb17d818515ac43772cc0b9b703d263ede3e
Martin Tjon <mtjon@users.noreply.github.com>
2024-06-12 07:39:57 +0200

    Add Rust template

--- flake.nix
+++ flake.nix
@@ -1,13 +1,26 @@
 {
 outputs = { self }: rec {
   templates = {
+    rust = { 
+      path = ./rust;
+      description = "Rust template";
+      welcomeText = ''
+        # Templated Rust project
+
+        This template provides the Rust language and LSP, cargo2nix for converting
+        your Cargo modules to Nix, and SOPS for secret management.
+
+        See `README.md` for further information.
+      '';
+    };
+
     go = {
       path = ./go;
-      description = "";
+      description = "Go template";
       welcomeText = ''
         # Templated Go project
 
-        This project provides the Go language and LSP, gomod2nix for converting
+        This template provides the Go language and LSP, gomod2nix for converting
         your Go modules to Nix, and SOPS for secret management.
 
         See `README.md` for further information.
---
+++ rust/flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Go project templated from github:mtjon/nix-flake-templates.";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+    naersk.url = "github:nix-community/naersk/master";
+  };
+  outputs = { self, nixpkgs, flake-utils, devshell, naersk }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlays.default ];
+        };
+        naersk-lib = pkgs.callPackage naersk { };
+        # Example of local packaged script
+        #my-script-buildInputs = with pkgs; [ curl ];
+        #my-script = (pkgs.writeScriptBin "my-script" (builtins.readFile ./scripts/my_script.sh)).overrideAttrs(old: {
+        #  buildCommand = "${old.buildCommand}\n patchShebangs $out";
+        #});
+      in
+      {
+        name = "Rust project.";
+
+        packages = {
+          # Example of local packaged script
+          #my-script = pkgs.symlinkJoin {
+          #  name = my-script;
+          #  paths = [ my-script ] ++ my-script-buildInputs;
+          #  buildInputs = [ pkgs.makeWrapper ];
+          #  postBuild = "wrapProgram $out/bin/my-script --prefix PATH : $out /bin";
+          #};
+          default = naersk-lib.buildPackage ./.;
+        };
+        apps = {};
+        devShells = {
+          default = pkgs.devshell.mkShell {
+            # Define the environment of your devshell
+            env = [
+            # e.g.
+            #{
+            #  name = "FOO";
+            #  eval = "$(sops -d ./secrets/local.yaml | yq '.foo')";
+            #}
+            ];
+
+            packages = with pkgs; [
+              cargo
+              rustc
+              rustfmt
+              rustPackages.clippy
+              sops # sops
+              age # used for sops encryption
+              # add further packages here, e.g.:
+              #openapi-generator-cli
+              #yq-go
+              # can also add local scripts from flake's packages
+              #my-script
+            ];
+          };
+        };
+      }
+    );
+}
+
