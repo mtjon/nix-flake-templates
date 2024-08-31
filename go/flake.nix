5c061ad469dc2dad93a9bceecd055a34e03102f0
Martin Tjon <mtjon@users.noreply.github.com>
2024-08-31 19:33:10 +0200

    Add crossBuild for Docker image

--- go/flake.nix
+++ go/flake.nix
@@ -23,16 +23,40 @@
           inherit system;
           overlays = [ devshell.overlays.default gomod2nix.overlays.default ];
         };
+        goAttrs = {
+          pname = "hello-go";
+          version = "0.1";
+          pwd = ./.;
+          src = ./.;
+          modules = ./gomod2nix.toml; 
+          vendorHash = null;
+        };
+        goBuild = pkgs.buildGoModule goAttrs;
+        goBuildCross = pkgs.pkgsCross.aarch64-multiplatform.buildGoModule goAttrs;
+        dockerImage = pkgs.dockerTools.buildLayeredImage {
+          # change name appropriately
+          name = "go-project";
+          tag = "0.1.0-snapshot";
+          # select appropriate architecture
+          architecture = "amd64";
+          # change Cmd appropriately
+          config.Cmd = [ "${goBuildCross}/bin/hello-world" ];
+          # Set the created to some meaningful date, breaking reproducibility
+          #created = "now";
+        };
         # Example of local packaged script
         #my-script-buildInputs = with pkgs; [ curl ];
         #my-script = (pkgs.writeScriptBin "my-script" (builtins.readFile ./scripts/my_script.sh)).overrideAttrs(old: {
         #  buildCommand = "${old.buildCommand}\n patchShebangs $out";
         #});
       in
-      {
+      rec {
         name = "Go project.";
 
         packages = {
+          go = goBuild;
+          docker = dockerImage;
+          default = packages.go;
           # Example of local packaged script
           #my-script = pkgs.symlinkJoin {
           #  name = my-script;
@@ -53,18 +77,19 @@
             #}
             ];
 
-            packages = with pkgs; [
-              go
-              gopls
-              gomod2nix
-              sops # sops
-              age # used for sops encryption
-              # add further packages here, e.g.:
-              #openapi-generator-cli
-              #yq-go
-              # can also add local scripts from flake's packages
-              #my-script
-            ];
+            packages = (with pkgs; [
+                go
+                gopls
+                sops # sops
+                age # used for sops encryption
+                # add further packages here, e.g.:
+                #openapi-generator-cli
+                #yq-go
+                # can also add local scripts from flake's packages
+                #my-script
+              ]) ++ [
+                gomod2nix.packages.${system}.default
+              ];
           };
         };
       }
